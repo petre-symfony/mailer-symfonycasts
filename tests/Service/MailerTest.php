dc1e424423bf460047875e6340d83bc3de2970da
petrero <petrero@gmail.com>
2019-12-26 09:20:39 +0200

    19.2.Unit Testing our Emails{Writing the Unit Test}
    Writing the Unit Test
    
    Let's get to work! So... what are we going to test? Well, we probably want to test that the mail was actually sent... and maybe we'll assert a few things about the Email object itself. Unit tests always start the same way: by instantiating the class you want to test.
    Back in MailerTest, rename the method to testSendWelcomeMessage()
    Then add $mailer = new Mailer(). For this to work, we need to pass the 4 dependencies: objects of the types MailerInterface, Twig, Pdf and EntrypointLookupInterface. In a unit test, instead of using real objects that really do send emails... or render Twig templates, we use mocks.
    For the first, say $symfonyMailer = this->createMock()... and because the first argument needs to be an instance of MailerInterface, that's what we'll mock: MailerInterface::class.
    To make sure we don't forget to actually send the email, we can add an assertion to this mock: we can tell PHPUnit that the send method must be called exactly one time. Do that with $symfonyMailer->expects($this->once()) that the ->method('send') is called.
    Let's create the 3 other mocks: $pdf = $this->createMock(Pdf::class)... and the other two are for Environment and EntrypointLookupInterface: $twig = $this->createMock(Environment::class) and $entrypointLookup = $this->createMock(EntrypointLookupInterface::class).
    These three objects aren't even used in this method... so we don't need to add any assertions to them or configure any behavior. Finish the new Mailer() line by passing $symfonyMailer, $twig, $pdf and $entrypointLookup. Then, call the method: $mailer->sendWelcomeMessage(). Oh, to do this, we need a User object.

--- tests/Service/MailerTest.php
+++ tests/Service/MailerTest.php
@@ -2,10 +2,23 @@
 
 namespace App\Tests\Service;
 
+use Knp\Snappy\Pdf;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\WebpackEncoreBundle\Asset\EntrypointLookupInterface;
+use Twig\Environment;
 
 class MailerTest extends TestCase {
-  public function testSomething(){
-    $this->assertTrue(true);
+  public function testSendWelcomeMessage(){
+	  $symfonyMailer = $this->createMock(MailerInterface::class);
+	  $symfonyMailer->expects($this->once())
+		  ->method('send');
+
+	  $pdf = $this->createMock(Pdf::class);
+	  $twig = $this->createMock(Environment::class);
+	  $entrypointLookup = $this->createMock(EntrypointLookupInterface::class);
+
+	  $mailer = new Mailer($symfonyMailer, $twig, $pdf, $entrypointLookup);
+	  $mailer->sendWelcomeMessage($user);
   }
 }
